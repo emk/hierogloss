4249d9020f5a07c023885bf95d392e9b46a881b2
Eric Kidd <git@randomhacks.net>
2014-01-20 07:37:17 -0500

    Remove auto-generated failing unit test

--- test/test_hierogloss.rb
+++ test/test_hierogloss.rb
@@ -4,8 +4,4 @@ class TestHierogloss < MiniTest::Unit::TestCase
   def test_that_it_has_a_version_number
     refute_nil ::Hierogloss::VERSION
   end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
