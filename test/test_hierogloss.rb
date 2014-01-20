d2a36bc5acc6ee67fe84069e8db522c3d8f640fe
Eric Kidd <git@randomhacks.net>
2014-01-20 07:38:01 -0500

    Fix deprecation warning in generated test suite

--- test/test_hierogloss.rb
+++ test/test_hierogloss.rb
@@ -1,6 +1,6 @@
 require 'minitest_helper'
 
-class TestHierogloss < MiniTest::Unit::TestCase
+class TestHierogloss < MiniTest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Hierogloss::VERSION
   end
